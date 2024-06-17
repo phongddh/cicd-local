5f6c6b0646301c2abf3fc7f6fc28eb771a07e402
phongddh <phong.daodh@gmail.com>
2024-06-17 13:53:35 +0700

    (config)-ansible-vagrant: update script

--- before_script.sh
+++ before_script.sh
@@ -1,3 +1,3 @@
 #ssh-keygen -t rsa -b 4096 -C "phong.daodh@gmail.com"
 ssh-keygen -f "/home/pong/.ssh/known_hosts" -R "192.168.56.50"
-cat /home/pong/.ssh/id_rsa.pub >> ./vagrant/keys
+cat /home/pong/.ssh/id_rsa.pub >> ./keys
